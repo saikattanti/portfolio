d1430792a9c4235cd8185789aacccb64890d983e
SAIKAT TANTI <saikat.tanti.fiem.cse23@teamfuture.in>
2024-11-08 01:04:41 +0530

    first commit

---
+++ src/constants/strings.ts
@@ -0,0 +1,38 @@
+class Strings {
+  static telegram = "Telegram";
+  static telegramLink = "https://telegram.me/SaikatTanti";
+  static telegramUsername = "Saikat Tanti";
+
+  static github = "GitHub";
+  static githubLink = "https://github.com/saikat-tanti";
+  static githubUsername = "saikat-tanti";
+
+  static linkedIn = "LinkedIn";
+  static linkedInLink = "https://www.linkedin.com/in/saikat-tanti-7375b2233/";
+  static linkedInUsername = "Saikat tanti";
+
+  static instagram = "Instagram";
+  static instagramLink = "https://www.instagram.com/saikattanti1/";
+  static instagramUsername = "saikattanti1";
+
+  static twitter = "Twitter";
+  static twitterLink = "https://x.com/TantiSaikat";
+  static twitterUsername = "TantiSaikat";
+
+  static email = "Email";
+  static primaryEmailLink = "saikattanti2005@gmail.com";
+  static primaryEmail = "saikattanti2005@gmail.com";
+
+  static buyMeACoffee = "BuyMeACoffee";
+  static buyMeACoffeeLink = "#";
+  static buyMeACoffeeUsername = "saikattanti";
+
+  static koFi = "Ko-Fi";
+  static koFiLink = "#";
+  static koFiUsername = "saikattanti";
+
+  static fullName = "Saikat Tanti";
+  static shortName = "saikattanti1";
+}
+
+export default Strings;
